553a9208c905c9f054cef901aafa46e3ca0353fc
ceo-s <alex_s_2002@mail.ru>
2025-07-06 19:28:53 +0800

    Add social networks bar

---
+++ scripts/social-container.js
@@ -0,0 +1,32 @@
+import debounce from "debounce";
+
+const trigger = document.querySelector(
+  "section#section-intro div.section-marker"
+);
+const scroller = document.querySelector("div.scroller");
+const hideMe = document.querySelector("#social-networks-container");
+hideMe.style.top = "-35px";
+
+console.log(trigger);
+console.log(hideMe);
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top < window.innerHeight &&
+    rect.bottom > 0 &&
+    rect.left < window.innerWidth &&
+    rect.right > 0
+  );
+}
+
+scroller.addEventListener(
+  "scroll",
+  debounce(() => {
+    if (isInViewport(trigger)) {
+      hideMe.style.top = "-35px";
+    } else {
+      hideMe.style.top = "0px";
+    }
+  }, 250)
+);
