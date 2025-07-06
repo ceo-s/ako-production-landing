7add74db490655a151fa4db78a0a5e1ee9e001bf
ceo-s <alex_s_2002@mail.ru>
2025-07-06 20:02:24 +0800

    Fix and check

--- functions/api/videos.js
+++ functions/api/videos.js
@@ -1,20 +1,21 @@
 export async function onRequest(context) {
   let res = {};
   let cursor = undefined;
+  const videos = undefined;
 
   try {
     while (!videos || !videos.list_complete) {
-      const videos = await context.env.AKO_VIDEOS_KV.list({ cursor });
+      videos = await context.env.AKO_VIDEOS_KV.list({ cursor });
       res = { ...res, ...videos.keys };
       cursor = videos.cursor;
     }
-    console.log("Balls");
-    console.log(videos.keys);
+
     return new Response(JSON.stringify(res), {
       status: 200,
     });
   } catch (err) {
-    console.log(err);
-    return new Response(e.message, { status: 500 });
+    return new Response(JSON.stringify({ message: err.message }), {
+      status: 500,
+    });
   }
 }
