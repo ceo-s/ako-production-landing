d1cfd3fae09eb9b4194fd17094bd780b3fc4ee7e
ceo-s <alex_s_2002@mail.ru>
2025-07-06 19:55:14 +0800

    Log errors cus wth

--- functions/api/videos.js
+++ functions/api/videos.js
@@ -8,10 +8,13 @@ export async function onRequest(context) {
       res = { ...res, ...videos.keys };
       cursor = videos.cursor;
     }
+    console.log("Balls");
+    console.log(videos.keys);
     return new Response(JSON.stringify(res), {
       status: 200,
     });
   } catch (err) {
+    console.log(err);
     return new Response(e.message, { status: 500 });
   }
 }
