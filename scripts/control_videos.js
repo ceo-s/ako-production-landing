c367c73a1566bd9e34b1e517495852806cf771b4
ceo-s <alex_s_2002@mail.ru>
2025-07-09 21:09:20 +0800

    Nearly final version

--- scripts/control_videos.js
+++ scripts/control_videos.js
@@ -1,21 +1,39 @@
 const player = document.getElementById("player-dialog");
+const video = player.querySelector("video");
+const playButton = player.querySelector(".play-button");
+const controls = player.querySelector(".controls");
+const stopButton = player.querySelector(".stop-button");
+const progress = player.querySelector(".progress");
+const fullViewButton = player.querySelector(".full-view-button");
 
 player.addEventListener("click", (e) => {
   if (e.target.tagName === "DIALOG") {
-    player.innerHTML = "";
+    e.stopPropagation();
+    pauseVideo();
     player.close();
   }
 });
 
+function playVideo() {
+  video.play();
+  playButton.style.display = "none";
+  controls.style.display = "flex";
+  progress.value = 0;
+  videoIsPlaying = true;
+}
+
+function pauseVideo() {
+  video.pause();
+  playButton.style.display = "inline-block";
+  controls.style.display = "none";
+  videoIsPlaying = false;
+}
+
 export function registerPlayer(videoContainer) {
   videoContainer.addEventListener("click", () => {
-    const name = videoContainer.children[1].innerHTML;
-    const link = videoContainer.firstChild.getAttribute("src");
-
-    player.innerHTML = `
-    <h4 class="video-name">${name}</h4>
-    <video src="${link}" controls="controls"></video>`;
-
+    video.setAttribute("src", videoContainer.firstChild.getAttribute("src"));
+    progress.value = 0;
+    pauseVideo();
     player.showModal();
   });
 }
@@ -31,3 +49,48 @@ export function registerAutoplay(video) {
     video.currentTime = 0;
   };
 }
+
+let videoIsPlaying = false;
+
+controls.style.display = "none";
+
+video.addEventListener("click", () => {
+  if (videoIsPlaying) {
+    pauseVideo();
+  } else {
+    playVideo();
+  }
+});
+
+playButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  playVideo();
+});
+
+stopButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  pauseVideo();
+});
+
+video.addEventListener("timeupdate", () => {
+  const value = (video.currentTime / video.duration) * 100;
+  progress.value = value;
+  console.log("Vals");
+});
+
+progress.addEventListener("input", () => {
+  const value = progress.value;
+  video.currentTime = (value / 100) * video.duration;
+});
+
+fullViewButton.addEventListener("click", () => {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) {
+    // Safari
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    // IE/Edge
+    video.msRequestFullscreen();
+  }
+});
