e051ca78507301f6a14aded96509b980591703d1
ceo-s <alex_s_2002@mail.ru>
2025-07-13 22:53:29 +0800

    Add pages and fix mobile video play

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
       input: {
         main: "index.html",
         production: "production/index.html",
+        "in-development": "in-development/index.html",
+        404: "404/index.html",
       },
     },
   },
